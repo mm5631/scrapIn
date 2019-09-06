60260812e8e984c8231afcf021c10f935f0fd6e2
Maxime <mm5631@nyu.ed>
2019-09-06 14:57:21 +0100

    resolving scraper.py commmits

--- src/scraper.py
+++ src/scraper.py
@@ -36,61 +36,12 @@ class Authenticator(Loader):
         signin = self.driver.find_element(by='class_name', value=self.search_params['signin_button'])
         signin.click()
 
-=======
-import os
-import time
-import argparse
-import numpy as np
-import pandas as pd
-from selenium import webdriver
-from bs4 import BeautifulSoup
-
-import logging
-log = logging.Logger(__name__)  # How to use?
-
-
-def argparser():
-    pass
-
-
-config = {
-    # Login page
-    'username_field': 'session_key',
-    'password_field': 'session_password',
-    'signin_button': 'sign-in-form__submit-btn',
-
-    # People search page
-    'filters_button': 'search-filters-bar__all-filters.flex-shrink-zero.mr3.artdeco-button.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view',
-    'job_title_field': 'search-advanced-title',
-    'apply_button': 'search-advanced-facets__button--apply.ml4.mr2.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view'
-    'previous_button': 'artdeco-pagination__button--previous',
-    'next_button': 'artdeco-pagination__button--next',
-    'search_container': '.core-rail'
-    'n_results_field': 'h3'
-}
->>>>>>> dce0acf8d9e831bb3ecbfb58848133cffc058735
-
 
 class Scraper:
-
-<<<<<<< HEAD
     def __init__(self, default_search=None):
-=======
-    def __init__(self, credentials, default_search=None):
->>>>>>> dce0acf8d9e831bb3ecbfb58848133cffc058735
-
         self.fail = 0
-        self._username = credentials[0]
-        self._password = credentials[1]
-<<<<<<< HEAD
-        self.driver = webdriver.Chrome(CHROMEDRIVER_FILEPATH)
-=======
-        self.driver = webdriver.Chrome('./chromedriver')
->>>>>>> dce0acf8d9e831bb3ecbfb58848133cffc058735
         self.default_search = default_search
         self.urls = []
-
-        self._authenticate()
 
     def make_request(self, func, config):
         try:
@@ -102,7 +53,7 @@ class Scraper:
             if self.fail <= 2:
                 time.sleep(2)
                 self.make_request(func, config)
-<<<<<<< HEAD
+
                 self.fail = 0
             else:
                 print(f'Could not retrieve element {config}, error:\n {e}')
@@ -111,37 +62,6 @@ class Scraper:
         print(f'[INFO] Scraping {job_function}')
 
         assert current_page == None
-=======
-            else:
-                print(f'Could not retrieve element {config}, error:\n {e}')
-
-    def _find_element(self, by, label):
-        params = {
-            'class_name': self.driver.find_element_by_class_name,
-            'id': self.driver.find_element_by_id
-            'css_selector': self.driver.find_element_by_css_selector,
-            'name': self.driver.find_element_by_name,
-            'tag_name': self.driver.find_element_by_tag_name
-        }
-        response = self.make_request(params[by], config[label])
-        return response
-
-    def _authenticate(self):
-        self.driver.get('https://www.linkedin.com')
-        time.sleep(2)
-        username = self._find_element(by='name', 'username_field')
-        password = self._find_element(by='name', 'password_field')
-
-        username.send_keys(self._username)
-        password.send_keys(self._password)
-
-        signin = self._find_element(by='class_name', 'signin_button')
-        signin.click()
-
-    def search_job_function(self, job_function):
-        print(f'[INFO] Scraping {job_function}')
->>>>>>> dce0acf8d9e831bb3ecbfb58848133cffc058735
-
         filters_button = self._find_element(by='class_name', 'filters_button')
         filters_button.click()
 
@@ -160,11 +80,7 @@ class Scraper:
 
     def get_n_results(self):
         try:
-<<<<<<< HEAD
             n_results = int(self._find_element(by='tag_name', 'n_results_field').text.split()[1].replace(',', ''))
-=======
-            n_results = np.int(self._find_element(by='tag_name', 'n_results_field').text.split()[1].replace(',', ''))
->>>>>>> dce0acf8d9e831bb3ecbfb58848133cffc058735
             return n_results
         except FileNotFoundError as e:
             print('No profiles meeting description')
